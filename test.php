559e64c51f0040fa6245e049561fc1d4d8f39c23
Katie Mangan <katie.mangan1@gmail.com>
2019-05-22 17:20:04 -0500

    declare variables and log string that includes declared variables

--- test.php
+++ test.php
@@ -1,4 +1,10 @@
 <?php 
-echo "Hello, World!";
+echo "Hello, World!\n";
+
+$first_name = "Katie";
+$last_name = "Mangan";
+$language = "PHP";
+
+echo "My name is $first_name $last_name and I'm learning all about $language today!\n"
 
 ?>
