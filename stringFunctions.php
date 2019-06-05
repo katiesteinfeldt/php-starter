886e0b27b679312803b0e1108a092b4e2dc3f478
Katie Mangan <katie.mangan1@gmail.com>
2019-06-05 11:29:12 -0500

    create stringFunction practice

---
+++ stringFunctions.php
@@ -0,0 +1,8 @@
+<?php
+
+echo strrev("Hello world!");
+
+echo strtolower("\n HELLO WORLD!!");
+
+echo str_repeat("\n Hello world! ", 3);
+?>
--- test.php
+++ test.php
@@ -5,6 +5,14 @@ $first_name = "Katie";
 $last_name = "Mangan";
 $language = "PHP";
 
-echo "My name is $first_name $last_name and I'm learning all about $language today!\n"
+echo "My name is $first_name $last_name and I'm learning all about $language today!\n";
+
+
+function tipCalculator($total, $tip = 20) 
+{
+    return $total + ($total * ($tip/100));
+}
+
+echo tipCalculator(100, 25);
 
 ?>
